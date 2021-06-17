3439feef24095fa4f2de310003461d5636939fd3
Stephen Odundo <steveremdi@gmail.com>
2021-06-17 14:44:34 +0300

    Added WeekOfTheMonth DLL functionality here (consolidation)

--- TimeBandChecker/TimeBandChecker.cs
+++ TimeBandChecker/TimeBandChecker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,6 +38,35 @@ namespace TimeBandChecker
                 time = time.Substring(0, time.Length - 2).Trim() + ":00" + time.Substring(time.Length - 2);
 
             return time;
+        }
+    }
+
+    public class WeekOfTheMonth
+    {
+        public int GetWeek(string date)
+        {
+            var isValidDate = DateTime.TryParseExact(date,
+                new string[] { "dd/MM/yyyy", "dd/M/yyyy", "d/MM/yyyy", "d/M/yyyy" },
+                new CultureInfo("en-US"), DateTimeStyles.None,
+                out DateTime dateObject);
+            if (!isValidDate)
+                return 0;
+
+            return dateObject.GetWeekOfMonth();
+        }
+    }
+
+    public static class DateTimeExtensions
+    {
+        private static GregorianCalendar _gc = new GregorianCalendar();
+        public static int GetWeekOfMonth(this DateTime time)
+        {
+            DateTime first = new DateTime(time.Year, time.Month, 1);
+            return time.GetWeekOfYear() - first.GetWeekOfYear() + 1;
+        }
+        public static int GetWeekOfYear(this DateTime time)
+        {
+            return _gc.GetWeekOfYear(time, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
         }
     }
 }
