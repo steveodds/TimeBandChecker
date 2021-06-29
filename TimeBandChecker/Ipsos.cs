6d7e3252911c29a11d399fbc9cf3f3fd5be5ebae
Stephen Odundo <steveremdi@gmail.com>
2021-06-29 21:42:49 +0300

    Added Ipsos data model

---
+++ TimeBandChecker/Ipsos.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeBandChecker
+{
+    internal class Ipsos
+    {
+        public string Row { get; set; }
+        public string IpsosTime { get; set; }
+        public string SubBrand { get; set; }
+    }
+}
