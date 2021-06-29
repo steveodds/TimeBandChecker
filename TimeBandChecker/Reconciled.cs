79f5e5a3779a05094cb659a95d741291d62c6ed9
Stephen Odundo <steveremdi@gmail.com>
2021-06-29 23:03:28 +0300

    Added model for reconciled records

---
+++ TimeBandChecker/Reconciled.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeBandChecker
+{
+    internal class Reconciled
+    {
+        public string AdamsRow { get; set; }
+        public string IpsosRow { get; set; }
+        public string Status { get; set; }
+
+    }
+}
