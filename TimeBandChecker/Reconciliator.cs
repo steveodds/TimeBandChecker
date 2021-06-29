efc6ce6ea8a61364b690f0f8cd6462873ad1b093
Stephen Odundo <steveremdi@gmail.com>
2021-06-29 19:33:32 +0300

    Added reconciliator class

---
+++ TimeBandChecker/Reconciliator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TimeBandChecker
+{
+    public class Reconciliator
+    {
+        private readonly List<string> _adamsData;
+        private readonly List<string> _ipsosData;
+        private List<string> finalList;
+        public Reconciliator(List<string> adamsData, List<string> ipsosData)
+        {
+            _adamsData = adamsData;
+            _ipsosData = ipsosData;
+            finalList = new List<string>();
+        }
+
+        public List<string> Reconcile()
+        {
+
+            return finalList;
+        }
+    }
+}
