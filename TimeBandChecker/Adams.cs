4d39d542925029bb25b2b36aacb710dde20c0935
Stephen Odundo <steveremdi@gmail.com>
2021-06-29 21:27:15 +0300

    Adams model

---
+++ TimeBandChecker/Adams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBandChecker
+{
+    class Adams
+    {
+        public string Row { get; set; }
+        public string VariantName { get; set; }
+        public string Material { get; set; }
+        public string TimebandStart { get; set; }
+        public string TimebandEnd { get; set; }
+        public bool HasTimeband { get; set; }
+        public string MappedSubBrand { get; set; }
+    }
+}
